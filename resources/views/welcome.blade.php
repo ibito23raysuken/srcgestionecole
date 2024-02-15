e64c69bcfb9db7355223291c236991b9066960fd
ibito23raysuken <ibitoraysuken@gmail.com>
2024-02-15 16:22:29 +0300

    modification de README

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,9 @@
     <div class="row">
       <div class="col-md-6 offset-md-3 text-center">
         <h1 class="display-4">Bienvenue dans notre application de gestion scolaire!</h1>
-        <p class="lead">Nous sommes ravis de vous accueillir dans notre application dédiée à simplifier et améliorer la gestion de notre école. Cette plateforme a été conçue pour offrir une expérience transparente et efficace, facilitant la collaboration entre les enseignants, les étudiants et les parents.</p>
+        <p class="lead">Nous sommes ravis de vous accueillir dans notre application dédiée à simplifier et améliorer
+            la gestion de notre école. Cette plateforme a été conçue pour offrir une expérience transparente et efficace,
+            facilitant la collaboration entre les enseignants, les étudiants et les parents.</p>
       </div>
 
   </div>
